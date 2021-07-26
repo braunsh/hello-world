f77875fd73fb8f328bb37e66f63b04a6d49ea7ec
Sharon Braun <braunsh@gmail.com>
2021-07-26 14:45:28 +0300

    move to function conponents and split to Todo and TodoList

--- todos-app/src/components/Todo.jsx
+++ todos-app/src/components/Todo.jsx
@@ -1,85 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react'
 
+export default function Todo({ index, indexOnEdit, text, checked, setEditMode, updateTodos }) {
 
-class Todo extends Component {
-    state = {
-        todos: [],
-        newTodoText: '',
-        indexOnEditMode: -1,
-    };
-
-    handleAddTodo = () => {
-        let itemToAdd = this.state.newTodoText.trim();
-        if (itemToAdd.length !== 0) {
-            let changeList = [...this.state.todos];
-            changeList[changeList.length] = { checked: false, text: itemToAdd };
-            this.setState({ todos: changeList });
-            this.setState({ newTodoText: " " });
-        }
+    const enableEditMode = (index) => {
+        setEditMode(index, true);
     }
 
-    handleDelete = (index) => {
-        let changeList = [...this.state.todos];
-        changeList.splice(index, 1);
-        this.setState({ todos: changeList });
-        this.setState({ indexOnEditMode: -1 });
+    const handleSaveChanges = (index) => {
+        setEditMode(false)
     }
 
-    handleSaveChanges = (index) => {
-        this.setState({ indexOnEditMode: -1 });
-    }
-
-
-    updateNewTodo = (event) => {
-        this.setState({ indexOnEditMode: -1 });
-        this.setState({ newTodoText: event.target.value });
-    }
-
-    setEditMode = (index) => {
-        this.setState({ indexOnEditMode: index });
-    }
-
-    handleEdit = (index, event) => {
-        let changeList = [...this.state.todos];
-        changeList[index] = { ...changeList[index], text: event.target.value };
-        this.setState({ todos: changeList });
-    }
-
-    handleCheckChange = (index, event) => {
-        let changeList = [...this.state.todos];
-        changeList[index] = { ...changeList[index], checked: event.target.checked };
-        this.setState({ todos: changeList });
-    }
-
-    renderTodoItem = (todo, index) => {
-        if (index === this.state.indexOnEditMode)
+    const renderTodoItem = () => {
+        if (index === indexOnEdit)
             return (
                 <>
-                    <input value={this.state.todos[index].text} onChange={(event) => this.handleEdit(index, event)} />
-                    <button className="btn btn-primary btn-sm m-1" onClick={() => this.handleSaveChanges(index)}>Save</button>
-                    <button className="btn btn-danger btn-sm m-1" onClick={() => this.handleDelete(index)}>X</button>
+                    <input value={text} onChange={(event) => updateTodos('update', index, 'text', event.target.value)} />
+                    <button className="btn btn-primary btn-sm m-1" onClick={() => handleSaveChanges(index)}>Save</button>
+                    <button className="btn btn-danger btn-sm m-1" onClick={() => updateTodos('delete', index, 'text')}>X</button>
                 </>);
         else return (
-            <p style={todo.checked ? { textDecorationLine: 'line-through' } : {}}>
-                <input type="checkbox" checked={todo.checked} onChange={event => this.handleCheckChange(index, event)} />
-                {"  "} {todo.text} </p>);
+            <p style={checked ? { textDecorationLine: 'line-through' } : {}}>
+                <input type="checkbox" checked={checked} onChange={event => updateTodos('update', index, 'checked', event.target.checked)} />
+                {"  "} {text} </p>);
     }
 
-    render() {
-        return (
-            <>
-                <h1>TODOs </h1>
-                <input value={this.state.newTodoText} onChange={this.updateNewTodo} />
-                <button className="btn btn-primary btn-sm m-2" onClick={this.handleAddTodo}>Add </button>
 
-                {this.state.todos.map((todo, index) =>
-                (
-                    <h6 key={index} onDoubleClick={() => this.setEditMode(index)} >
-                        {this.renderTodoItem(todo, index)}
-                    </h6>
-                ))}
-            </>);
-    }
+    return (
+        <h6 key={index} onDoubleClick={() => enableEditMode(index)} >
+            {renderTodoItem()}
+        </h6>
+    );
 }
-
-export default Todo;
---
+++ todos-app/src/components/TodoList.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+
+import Todo from '../components/Todo';
+
+
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+    const [addTodoText, setAddTodoText] = useState('');
+    const [indexOnEdit, setIndexOnEdit] = useState(-1);
+
+    const handleAddTodo = () => {
+        let textToAdd = addTodoText.trim();
+        if (textToAdd.length !== 0) {
+            let newTodos = [...todos];
+            newTodos[newTodos.length] = { checked: false, text: textToAdd };
+            setTodos(newTodos);
+            setAddTodoText('');
+        }
+    }
+
+    const handleDelete = (index) => {
+        let newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+        setEditMode(false)
+    }
+
+    const updateNewTodoText = (event) => {
+        setAddTodoText(event.target.value);
+    }
+
+    const setEditMode = (index = -1, isEnabled) => {
+        if (isEnabled && index === -1)
+            console.error("Error enabling edit mode: index should be set when edit moe is enabled");
+        setIndexOnEdit(isEnabled ? index : index);
+    }
+
+    const updateTodos = (action, indexToUpdate, field, value) => {
+        switch (action) {
+            case 'delete':
+                handleDelete(indexToUpdate);
+                break;
+            case 'update':
+                handleUpdate(indexToUpdate, field, value)
+                break;
+            default:
+                console.error("action " + action + " not supported on updateTodos");
+        }
+    }
+
+
+    const handleUpdate = (index, field, value) => {
+        let newTodos = [...todos];
+
+        switch (field) {
+            case 'text':
+                newTodos[index] = { ...newTodos[index], text: value };
+                break;
+            case 'checked':
+                newTodos[index] = { ...newTodos[index], checked: value };
+                break;
+            default:
+                console.error("field " + field + " not supported on handleUpdate");
+        }
+        setTodos(newTodos);
+    }
+
+
+    return (
+        <>
+            <h1>TODOs </h1>
+            <input value={addTodoText} onChange={updateNewTodoText} />
+            <button className="btn btn-primary btn-sm m-2" onClick={handleAddTodo}>Add </button>
+
+            {todos.map((todo, index) => <Todo index={index} {...todo}
+                indexOnEdit={indexOnEdit}
+                handleDelete={handleDelete}
+                setEditMode={setEditMode}
+                updateTodos={updateTodos} />
+            )}
+
+        </>);
+}
+
+export default TodoList;
+
